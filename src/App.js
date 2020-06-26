318e3d1a0db16dfb5ba0dd1d13ab370f0ae585db
jys77 <jiang-yunsheng@hotmail.com>
2020-06-25 23:53:21 -0400

    init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  color: blue;
+`;
+
+const App = () => {
+  return <Wrapper>Yo</Wrapper>;
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+    ],
+  },
+  devtool: "cheap-module-eval-source-map",
+  devServer: {
+    contentBase: path.resolve(__dirname, "public"),
+  },
+};
